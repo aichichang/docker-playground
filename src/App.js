e9d3531d7d7404cf73d862ebaaf6939664942055
aichichang <aichi.chang@nandos.co.uk>
2020-10-03 21:50:28 +0100

    add new feature

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Hej Hej!!?
+          New change for the nwe-feature branch
         </p>
         <a
           className="App-link"
